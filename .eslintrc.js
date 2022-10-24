bb63c4c4d1f9cc173609a1029b648fdf147f19d2
unknown <ghks2047@naver.com>
2022-10-24 11:37:59 +0900

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,14 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
   },
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'prettier',
   ],
   parserOptions: {
     parser: '@babel/eslint-parser',
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports={
+  semi:true, // 세미콜론 사용 여부
+  trailingComma:'all', // 여러줄 사용시 후행 콤마 사용 방식
+  singleQuote:true, // single 쿼테이션 사용 여부
+  printWidth:100,// 줄바꿈 폭 길이
+  tabWidth:2, // 탭 너비
+  endOfLine:'auto', // EOF 방식
+  useTabs:false, // 탭 사용 여부
+}
