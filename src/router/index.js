91b59179dd0cb43e4364fc24f99f747e01bc58c9
unknown <ghks2047@naver.com>
2022-10-25 15:32:39 +0900

    feat: todo 페이지를 라우터에 등록

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import TodoView from '@/views/TodoView.vue';
 import HomeView from '../views/HomeView.vue';
 
 Vue.use(VueRouter);
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView,
+  },
+  {
+    path: '/todo',
+    name: 'todo',
+    component: TodoView,
   },
   {
     path: '/about',
