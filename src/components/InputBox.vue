23bd1a0cb213bd4c855ab41a1588969bcdf65236
unknown <ghks2047@naver.com>
2022-10-25 12:34:17 +0900

    feat: InputBox border 옵션 추가

--- src/components/InputBox.vue
+++ src/components/InputBox.vue
@@ -1,76 +1,146 @@
 <template>
-  <div class="input-box">
-    <input
-      v-model="name"
-      class="input-box__input--normal"
-      :class="{'input-box__input--focused':isActive, 'input-box__input--error':isError}"
-      placeholder="Input out name"
-      @focus="focusCheck"
-    />
-    <img v-if="isActive" class="input-box__img" src="../assets/vector.svg" alt="none">
-    <img v-else class="input-box__img" src="../assets/disabled_vector.svg" alt="none">
+  <div class="container-col">
+    <div class="container-row">
+      <div
+        class="container-row input-box"
+        :class="{
+          'input-box__input--focused': isFocus,
+          'input-box__input--error': isError,
+          'input-box__input--border': delBorder,
+        }"
+      >
+        <input
+          v-model="name"
+          class="input-box__input--normal"
+          placeholder="Input out name"
+          @focusin="focus(true)"
+          @focusout="focus(false)"
+        />
+        <DeleteIcon v-show="isText" class="input-box__delete" @click. native="delName"></DeleteIcon>
+      </div>
+      <SendHovIcon v-if="canSend" class="input-box__send"/>
+      <SendNorIcon v-else class="input-box__send"/>
+    </div>
     <div v-if="isError" class="input-box__error-message">error message</div>
   </div>
 </template>
 
 <script>
-export default {
-  // eslint-disable-next-line vue/multi-word-component-names
-  name: 'InputBox',
-  props: {
-  },
-  data(){
-    return{
-      isActive:false,
-      isError:false,
-      name:""
-    }
-  },
-  watch:{
-    name() {
-      const reg = /^[a-z|A-Z]*$/;
-      if (!reg.test(this.name)) { // 특수문자 들어간 경우
-        this.isError=true;
-        this.isActive=false;
-      }
-      else{
-        this.isError=false;
-        this.isActive=true;
-      }
-    }
-  },
-  methods:{
-    focusCheck(){
-      if(this.isError){
-        // 문제 있으므로 focus 작용안함
-      }
-      else{
-        this.isActive=true;
-      }
-    }
-  }
+import SendNorIcon from '@/components/icon/SendNorIcon.vue';
+import SendHovIcon from '@/components/icon/SendHovIcon.vue';
+import DeleteIcon from '@/components/icon/DeleteIcon.vue';
 
-};
+export default {
+  name: 'InputBox',
+  components:{
+    SendNorIcon, SendHovIcon,DeleteIcon
+  },
+  props: {
+    noUseBorder: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      name: '',
+      isFocus: false,
+      isText: false,
+      canSend: false,
+      isError: false,
+
+      delBorder:this.noUseBorder // props에서 받은 데이터를 사용가능하도록 받아준다.
+    };
+  },
+  watch: {
+    name() {
+      // 글자수에 따른 액션
+      if (this.name.length === 0) {
+        this.isText = false;
+        this.canSend = false;
+        this.isError = false;
+        return;
+      }
+      this.isText = true;
+
+      // 특수문자 체크
+      const reg = /^[a-z|A-Z]*$/;
+      if (!reg.test(this.name)) {
+        // 특수문자 들어간 경우
+        this.isError = true;
+        this.canSend = false;
+      } else {
+        this.isError = false;
+        this.canSend = true;
+        this.isFocus = true;
+      }
+    },
+  },
+  methods: {
+    focus(isIn) {
+      if (isIn) {// 포커스시도
+        if (!this.isError) {
+          this.isFocus = true;
+        }
+      } else {// 포커스 아웃시도
+        this.isFocus = false;
+      }
+    },
+    delName() {
+      this.name = '';
+      this.canSend = false;
+    },
+  }};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss">
-.container {
-  height: 40px ;
+<style scoped lang="scss">
+.container-row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
-input{  width: 648px;
-  height: 32px;
+.container-col {
+  display: flex;
+  flex-direction: column;
+}
+
+input {
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: 0em;
+  text-align: left;
+
+  width: 648px;
   border: none;
-  border-bottom: 1px solid #CCCCCC;
+  outline: none;
 }
-.input-box{
-  &__input--focused{
-    border-bottom: 1px solid #2A82F0;
+.input-box {
+  width: 648px;
+  height: 32px;
+  border: 1px solid;
+  border-color: #cccccc;
+  border-radius: 4px;
+
+
+  &__input--border {
+    //border: 1px solid #CCCCCC;
+    //border: 1px solid;
+    border-left-width: 0px;
+    border-right-width: 0px;
+    border-top-width: 0px;
+    border-radius: 0px;
+
   }
-  &__input--error{
-    border-bottom: 1px solid #D15050;
+
+  &__input--focused {
+    border-color: #2a82f0;
   }
-  &__error-message{
+  &__input--error {
+    border-color: #d15050;
+  }
+  &__error-message {
     font-family: Roboto;
     font-size: 12px;
     font-weight: 400;
@@ -78,11 +148,15 @@ input{  width: 648px;
     letter-spacing: 0em;
     text-align: left;
 
-    color: #D15050;
+    color: #d15050;
   }
-  &__img{
-
+  &__delete {
+    width: 20px;
+    height: 20px;
+    padding-right: 5px;
+  }
+  &__send {
+    padding-left: 11px;
   }
 }
-
 </style>
