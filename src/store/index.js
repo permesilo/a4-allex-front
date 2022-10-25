c6bb548ed3d40038bbbe7996b7e2a6468527f538
unknown <ghks2047@naver.com>
2022-10-25 13:45:25 +0900

    feat: vuex를 mapMutations로 사용가능한지 리렌더링이 가능한지 확인함

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,18 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    userName:"임시"
   },
-  getters: {
+  getters: { // 사용자가 연산한 결과를 반환받고 싶을떄 사용
   },
-  mutations: {
+  mutations: { // state의 상태를 바꾸기 위해 사용
+    setUserName(state, name){
+      console.log("vuex actions setUserName");
+      state.userName = name;
+    }
   },
-  actions: {
+  actions: { // 비동기처리(rest api)에 사용
   },
-  modules: {
+  modules: { // 여거 store를 관리하고 싶을 떄 사용
   },
 });
